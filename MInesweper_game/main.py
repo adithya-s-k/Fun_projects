23aed83de96cc8dd433e8a625794aedf280e9693
Adithya S K <adithyaskolavi@gmail.com>
2022-09-20 20:29:07 +0530

    minsweeper game using Python and CMD complete - Adithya S K

--- MInesweper_game/main.py
+++ MInesweper_game/main.py
@@ -1,7 +1,7 @@
 # an command line based minesweeper game to understand on how to deal with 2d arrays
 
 import random
-
+import re
 class Board:
     def __init__(self,dim_size,num_bombs):
         self.dim_size = dim_size
@@ -48,9 +48,86 @@ class Board:
     def dig(self,row,col):
         self.dug.add((row,col))
         
-        if 
+        if self.board[row][col] == '*':
+            return False
+        elif self.board[row][col] > 0:
+            return True
+        
+        for r in range(max(0,row-1), min(self.dim_size-1,row+1)+1):
+            for c in range(max(0,col-1), min(self.dim_size-1,col+1)+1):
+                if(r,c) in self.dug:
+                    continue
+                self.dig(r,c)
+                
+        return True
+
+    def __str__(self):
+        visible_board = [[None for _ in range(self.dim_size)] for _ in range(self.dim_size)]
+        for row in range(self.dim_size):
+            for col in range(self.dim_size):
+                if (row,col) in self.dug:
+                    visible_board[row][col] = str(self.board[row][col])
+                else:
+                    visible_board[row][col] = ' '
+        
+        string_rep = ''
+        widths = []
+        for idx in range(self.dim_size):
+            columns = map(lambda x: x[idx], visible_board)
+            widths.append(
+                len(
+                    max(columns, key = len)
+                )
+            )
+
+        indices = [i for i in range(self.dim_size)]
+        indices_row = '   '
+        cells = []
+        for idx, col in enumerate(indices):
+            format = '%-' + str(widths[idx]) + "s"
+            cells.append(format % (col))
+        indices_row += '  '.join(cells)
+        indices_row += '  \n'
+        
+        for i in range(len(visible_board)):
+            row = visible_board[i]
+            string_rep += f'{i} |'
+            cells = []
+            for idx, col in enumerate(row):
+                format = '%-' + str(widths[idx]) + "s"
+                cells.append(format % (col))
+            string_rep += ' |'.join(cells)
+            string_rep += ' |\n'
+
+        str_len = int(len(string_rep) / self.dim_size)
+        string_rep = indices_row + '-'*str_len + '\n' + string_rep + '-'*str_len
+
+        return string_rep
+        
 
 def play(dim_size = 10, num_bombs = 10):
     
     board = Board(dim_size,num_bombs)
-    pass
+    
+    while len(board.dug) < board.dim_size ** 2 - num_bombs:
+        print(board)
+        user_input = (input("Where would you like to dig?\n Input as row,column")).split(",")
+        row, col = int(user_input[0]) , int(user_input[-1])
+        if row < 0 or row>= board.dim_size or col<0 or col>= dim_size:
+            print("Invalid laction . try again")
+            continue
+        
+        safe = board.dig(row,col)
+        if not safe:
+            break
+        
+    if safe:
+        print("Congratulations")
+    else:
+        print("Sorry game over")
+        
+        board.dug = [(r,c) for r in range(board.dim_size) for c in range(board.dim_size)]
+        print(board)
+        
+if __name__ == "__main__":
+    play()
